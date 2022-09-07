3885ff5b88437f0dcd707eb9c9c52811ce64f364
Filip Kubiś <filip.kubis@stazysta.comarch.pl>
2022-09-07 17:09:01 +0200

    FK - security class refactor

--- ms-customers/src/main/java/com/comarch/microservices/customers/utils/Security.java
+++ ms-customers/src/main/java/com/comarch/microservices/customers/utils/Security.java
@@ -3,11 +3,14 @@ package com.comarch.microservices.customers.utils;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
 public class Security {
-
+    private Security(){
+        throw new IllegalStateException("Utility class!");
+    }
     public static final String EMAIL_REGEX = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\."
-            +"[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@"
-            +"(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
-    public static String hashPassword(String password){
+            + "[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@"
+            + "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+
+    public static String hashPassword(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt());
     }
 }
