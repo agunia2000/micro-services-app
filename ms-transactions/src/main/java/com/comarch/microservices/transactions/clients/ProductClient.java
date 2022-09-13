f934866c4f0d8c110e2598bb5908a03c32a487c6
Filip Kubiś <filip.kubis@stazysta.comarch.pl>
2022-09-13 13:30:53 +0200

    FK - ms-transactions, check if product exists while doing transactions

--- ms-transactions/src/main/java/com/comarch/microservices/transactions/clients/ProductClient.java
+++ ms-transactions/src/main/java/com/comarch/microservices/transactions/clients/ProductClient.java
@@ -8,6 +8,5 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "ms-products")
 public interface ProductClient {
     @GetMapping("/products/{code}")
-    //public List<ProductResponse> getProducts();
     public ProductResponse getProductByCode(@PathVariable("code") String code);
 }
