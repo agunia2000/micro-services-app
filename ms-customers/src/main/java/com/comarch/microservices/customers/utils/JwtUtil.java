84b0b04311e27b047366ee5f4355b108212c160d
guniaa <amadeusz.gunia@stazysta.comarch.pl>
2022-09-14 11:07:41 +0200

    refactoring

--- ms-customers/src/main/java/com/comarch/microservices/customers/utils/JwtUtil.java
+++ ms-customers/src/main/java/com/comarch/microservices/customers/utils/JwtUtil.java
@@ -24,16 +24,6 @@ public class JwtUtil {
 
     private final long tokenValidity = 7 * 24 * 60 * 60; // in seconds, here: one week = 7 days
 
-    public Claims getClaims(final String token) {
-        try {
-            Claims body = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
-            return body;
-        } catch (Exception e) {
-            System.out.println(e.getMessage() + " => " + e);
-        }
-        return null;
-    }
-
     public String generateToken(String email) {
         Claims claims = Jwts.claims().setSubject(email);
         long nowMillis = System.currentTimeMillis();
@@ -41,22 +31,6 @@ public class JwtUtil {
         Date exp = new Date(expMillis);
         return Jwts.builder().setClaims(claims).setIssuedAt(new Date(nowMillis)).setExpiration(exp)
                 .signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
-    }
-
-    public void validateToken(final String token) throws JwtTokenMalformedException, JwtTokenMissingException {
-        try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
-        } catch (SignatureException ex) {
-            throw new JwtTokenMalformedException("Invalid JWT signature");
-        } catch (MalformedJwtException ex) {
-            throw new JwtTokenMalformedException("Invalid JWT token");
-        } catch (ExpiredJwtException ex) {
-            throw new JwtTokenMalformedException("Expired JWT token");
-        } catch (UnsupportedJwtException ex) {
-            throw new JwtTokenMalformedException("Unsupported JWT token");
-        } catch (IllegalArgumentException ex) {
-            throw new JwtTokenMissingException("JWT claims string is empty.");
-        }
     }
 
 }
